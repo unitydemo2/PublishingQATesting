4d0260a41ac14939b80d70bc7e87546c52f94366
CMSQATeam <cmsqa123@gmail.com>
2019-02-05 15:43:29 +0530

    DW CMS Message

--- ScriptRef/Model/EditLengthResult.cs
+++ ScriptRef/Model/EditLengthResult.cs
@@ -15,15 +15,22 @@ namespace DiffPlex.Model
         InsertUp
     }
 
+    
     public class EditLengthResult
     {
+        
         public int EditLength { get; set; }
 
+        
         public int StartX { get; set; }
+        
         public int EndX { get; set; }
+        
         public int StartY { get; set; }
+        
         public int EndY { get; set; }
 
+        
         public Edit LastEdit { get; set; }
     }
 }
