9a581501c2040c54c93fb63a3f242677ea46f5c3
CMSQATeam <cmsqa123@gmail.com>
2019-02-06 14:45:58 +0530

    DW CMS Message

---
+++ Images/Examples/DateTimezone_5c3c392b810f282808e52ca9.cs
@@ -0,0 +1,5 @@
+public DateInterval diff ( DateTimeInterface $datetime2 [, bool $absolute = FALSE ] )
+public string format ( string $format )
+public int getOffset ( void )
+public int getTimestamp ( void )
+public DateTimeZone getTimezone ( void )
--- ScriptRef/DiffBuilder/Model/ClassProperty.cs
+++ ScriptRef/DiffBuilder/Model/ClassProperty.cs
@@ -1,15 +1,39 @@
 namespace DiffPlex.DiffBuilder.Model
 {
     
+    /// <summary>
+    /// Use the Unity Editor to create 2D and 3D games
+    /// </summary>
+    /// <description>
+    /// ![An Image control](ImageCtrlExample_5c387c7fed64cc1190cc0ae8.png)
+    /// </description>
     public class ClassProperty
     {
         
+        /// <summary>
+        /// # VisionUtility
+        /// </summary>
+        /// <description>
+        /// [5c3c392b810f282808e52ca9](DateTimezone_5c3c392b810f282808e52ca9.cs)
+        /// </description>
         public int NumberA { get; set; }
 
         
+        /// <summary>
+        /// A class containing methods to assist with accessibility for users with different vision capabilities
+        /// </summary>
+        /// <description>
+        /// A class containing methods to assist with accessibility for users with different vision capabilities
+        /// </description>
         public int NumberB { get; set; }
 
         
+        /// <summary>
+        /// **palette**
+        /// </summary>
+        /// <description>
+        /// []()
+        /// </description>
         public int NumberC { get; set; }
     }
 }
