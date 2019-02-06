02076c33ca2ba52d15c0d91bdedf8d5537ae458e
CMSQATeam <cmsqa123@gmail.com>
2019-02-06 11:36:24 +0530

    DW CMS Message

--- ScriptRef/Log.cs
+++ ScriptRef/Log.cs
@@ -2,14 +2,17 @@ using System.Diagnostics;
 
 namespace DiffPlex
 {
+    
     public static class Log
     {
+        
         [Conditional("LOG")]
         public static void WriteLine(string format, params object[] args)
         {
             Debug.WriteLine(string.Format(format, args));
         }
 
+        
         [Conditional("LOG")]
         public static void Write(string format, params object[] args)
         {
