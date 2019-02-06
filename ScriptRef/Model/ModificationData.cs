c1eae57c6e920d15e9c981b9e128560e52d3a7b8
CMSQATeam <cmsqa123@gmail.com>
2019-02-06 11:36:11 +0530

    DW CMS Message

--- ScriptRef/Model/ModificationData.cs
+++ ScriptRef/Model/ModificationData.cs
@@ -1,15 +1,21 @@
-﻿namespace DiffPlex.Model
+namespace DiffPlex.Model
 {
+    
     public class ModificationData
     {
+        
         public int[] HashedPieces { get; set; }
 
+        
         public string RawData { get; }
 
+        
         public bool[] Modifications { get; set; }
 
+        
         public string[] Pieces { get; set; }
 
+        
         public ModificationData(string str)
         {
             RawData = str;
